5e6116017e04dc27d6ecf861763e77c680923b9f
github username <github email address>
2017-11-25 23:29:39 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 # Add seed data here. Seed your database with `rake db:seed`
+bruce = Artist.create(name: "Bruce Springsteen")
 
+born= Song.create(name: "Born to Run", artist_id: bruce.id)
+
+rock = Genre.create(name: "Rock")
+pop = Genre.create(name:"Pop")
+
+
+br = SongGenre.create(song_id: born.id, genre_id: rock.id)
